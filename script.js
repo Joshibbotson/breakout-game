02bfd5f79b499d43bf29d684cb91896d5c8bf2a4
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-10-26 21:34:52 +0100

    updated but lack of understanding toward co-ordinates so need to figure that out

--- script.js
+++ script.js
@@ -4,8 +4,44 @@ const ctx = canvas.getContext("2d")
 let x = canvas.width / 2
 let y = canvas.height - 30
 
-let dx = -4
+let dx = -1
 let dy = -3
+
+let rightPressed = false
+let leftPressed = false
+
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = true
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = true
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = false
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = false
+    }
+}
+
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+    ctx.fillStyle = "black"
+    ctx.fill()
+    ctx.closePath()
+}
+
+drawPaddle()
 
 const ballRadius = 10
 
@@ -19,15 +55,29 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall()
-    x += dx
-    y += dy
+    drawPaddle()
 
-    // checks if ball is hitting the wall via co-ordinates
-    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
-        dy = -dy
-    } else if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx
     }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy
+    }
+
+    if (rightPressed) {
+        paddleX += 7
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth
+        }
+    } else if (leftPressed) {
+        paddleX -= 7
+        if (paddleX < 0) {
+            paddleX = 0
+        }
+    }
+
+    x += dx
+    y += dy
 }
 
 // refreshes animation every 10 milliseconds
